72fe81e2229f9c48c97aa2a4c8aa69ef8320f8ba
kiran187 <kirantupe1112@gmail.com>
2024-12-07 10:12:16 +0530

    Update connection.js

--- routes/connection.js
+++ routes/connection.js
@@ -5,7 +5,7 @@ var conn = mysql.createConnection({
     "host":"bwbhldwydzajlkylvt2i-mysql.services.clever-cloud.com",
     "user":"urh9qzgrwzh4mqcb",
     "password":"FRFG300Uo8uL0It0cjHm",
-    "database":"ultras_nodejs"
+    "database":"bwbhldwydzajlkylvt2i"
 });
 
 var exe = util.promisify(conn.query).bind(conn);
